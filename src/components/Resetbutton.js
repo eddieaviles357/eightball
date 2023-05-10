740e9d1fbc22cec11b2d2ceb30f35160fc4f0a6d
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-10 11:14:44 -0700

    added a reset button that will reset the status of the Eightball to original state

---
+++ src/components/Resetbutton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Resetbutton = ({action}) => {
+    return (
+        <button className='Resetbutton' onClick={action}>Reset</button>
+    )
+}
+
+export default Resetbutton;
